6b7b52b94ca098606550126979b991cb10f535ae
Oscar <correo@salazaryabogadosasociados.com>
2026-03-23 23:56:44 -0500

    Diseño estilo Arrubla

--- public/script.js
+++ public/script.js
@@ -6,21 +6,3 @@ function toggleMenu() {
         menu.style.display = "flex";
     }
 }
-
-document.addEventListener("mousemove", function(e) {
-    let trail = document.createElement("div");
-    trail.style.position = "absolute";
-    trail.style.left = e.pageX + "px";
-    trail.style.top = e.pageY + "px";
-    trail.style.width = "10px";
-    trail.style.height = "10px";
-    trail.style.background = "rgba(201,166,70,0.7)";
-    trail.style.borderRadius = "50%";
-    trail.style.pointerEvents = "none";
-    
-    document.body.appendChild(trail);
-    
-    setTimeout(() => {
-        trail.remove();
-    }, 300);
-});
